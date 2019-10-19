dd0981f2cb5a486cc9fd6666d94009f44b70fcb9
Xola <xola@xolas-mbp.lan>
2019-10-19 07:43:31 -0700

    Add new calc console app

---
+++ SimpleHelloWorldTestWithGit/SimpleHelloWorldTestWithGit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleHelloWorldTestWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Add(int a, int b)
+        {
+            throw new NotImplementedException("Someone please impliment this");
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            throw new NotImplementedException("Someone please impliment this");
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            throw new NotImplementedException("Someone please impliment this");
+        }
+
+        public static int Division(int a, int b)
+        {
+            throw new NotImplementedException("Someone please impliment this");
+        }
+    }
+}
